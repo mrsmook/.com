c0bdb8396416eae991249808773ea0010f80ae4c
ext-dev-060319 <ext-dev-060319@beauteprivee.com>
2019-05-21 16:52:13 +0200

    config change

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ try {
 
 const {
   NODE_ENV,
-  URL: NETLIFY_SITE_URL = 'https://imselim.ml',
+  URL: NETLIFY_SITE_URL = 'https://imselim.netlify.com',
   DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
   CONTEXT: NETLIFY_ENV = NODE_ENV,
 } = process.env
